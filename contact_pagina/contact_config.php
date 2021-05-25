43fb888319dcaf2e23792dfef57d1fe3e520b34e
alicia573 <54794745+alicia573@users.noreply.github.com>
2021-05-25 09:50:45 +0200

    Contact form

---
+++ contact_pagina/contact_config.php
@@ -0,0 +1,28 @@
+<?php
+    $message_sent="false";
+    if(isset($_POST['submit'])){
+        $name= $_POST['name'];
+        $last_name= $_POST['last_name'];
+        $phonenumber= $_POST['number'];
+        $email= $_POST['mail'];
+        $subject= $_POST['subject'];
+        $message= $_POST['message'];
+
+        if(filter_var($_POST['mail'],FILTER_VALIDATE_EMAIL)){
+
+            $to= "bitprojectgroep1@gmail.com,$email";
+
+            $message_sent = mail($to,$subject,$message, 'From: ' . $email);
+
+            if ($message_sent) {
+                header('Contact.html');
+            } else {
+                exit("Niet verzonden, probeer het opnieuw.");
+            }
+
+        }else{
+
+            echo $invalid_class_name= "form-invalid";
+        }
+
+}
