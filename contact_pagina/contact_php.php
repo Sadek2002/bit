de13eb9ee6c0cb8761f38567e221d7f655fa6bc6
alicia573 <54794745+alicia573@users.noreply.github.com>
2021-05-25 13:23:37 +0200

    update

--- contact_pagina/contact_php.php
+++ contact_pagina/contact_php.php
@@ -16,7 +16,7 @@ if (isset($_POST['submit'])) {
         $message_sent = mail($to, $subject, $message, 'From: ' . $email);
 
         if ($message_sent) {
-            header('Contact.html');
+            header('emailhasbeensent.php');
         } else {
             exit("Niet verzonden, probeer het opnieuw.");
         }
