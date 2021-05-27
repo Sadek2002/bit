1f1fc5ac36866ee0ce8468332689ac44d62ce0e7
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2021-05-27 10:41:24 +0200

    added explanation comments

--- php/database test file.php
+++ php/database test file.php
@@ -1,15 +1,18 @@
 <?php
 
+//this way you need to activate de Database.php file
 require_once "Database.php";
 $db = new Database();
 $db->checkConnectionToDatabase();
 
 
-/*echo "<pre>";
-print_r($db->getTableByName("products"));
-echo "</pre>";*/
+//echo "<pre>";
+//print_r($db->getTableByName("products"));
+//echo "</pre>";
 
-foreach ($db->getTableByName("products") as $row) {
+
+//this way you can use getTableByName
+foreach ($db->getTableByName("admins") as $row) {
     foreach ($row as $item => $value) {
         if ($item === "$item") {
             echo $item.": ".$value;
