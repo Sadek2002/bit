56841d30988fa3a50b55872014c5add64d76ee0e
alicia573 <54794745+alicia573@users.noreply.github.com>
2021-06-01 10:40:39 +0200

    update

--- Productpage/test.php
+++ Productpage/test.php
@@ -18,25 +18,25 @@ $db->checkConnectionToDatabase();
 
 
 <header>
+    <nav id= "menu">
+        <ul>
+            <li><img id="bit-img" src="../img/logo.svg"></li>
+            <!--<li style="float: right; padding-right: 30px;" ><a href = "Contact.html" target = "_self">contact</a></li>-->
+            <li style = "float: right"><a href = "">basket</a></li>
+            <li style = "float: right"><a href = "">acount</a></li>
+            <li style = "float: right"><a href = "">search</a></li>
+            <li class = "dropdown" style="float: right;">
+                <a href = "javascript:void(0)" class="dropbtn">Categorie&#235;n</a>
+                <div class="dropdown-content">
+                    <a href="#">link 1</a>
+                    <a href="#">link 2</a>
+                </div>
+            </li>
+            <li style = "float: right"><a href="index.html" target = "_self">Shop</a></li>
+        </ul>
+    </nav>
 
 </header>
-<nav id= "menu">
-    <ul>
-        <li><img id="bit-img" src="../img/logo.svg"></li>
-        <!--<li style="float: right; padding-right: 30px;" ><a href = "Contact.html" target = "_self">contact</a></li>-->
-        <li style = "float: right"><a href = "">basket</a></li>
-        <li style = "float: right"><a href = "">acount</a></li>
-        <li style = "float: right"><a href = "">search</a></li>
-        <li class = "dropdown" style="float: right;">
-            <a href = "javascript:void(0)" class="dropbtn">Categorie&#235;n</a>
-            <div class="dropdown-content">
-                <a href="#">link 1</a>
-                <a href="#">link 2</a>
-            </div>
-        </li>
-        <li style = "float: right"><a href="index.html" target = "_self">Shop</a></li>
-    </ul>
-</nav>
 
 
 <div class="row">
