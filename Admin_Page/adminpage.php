e0ce661b387f419751e526a4177de66585cb8eb9
Sadek <39184773+Sadek2002@users.noreply.github.com>
2021-06-03 18:59:23 +0200

    Added function to send data to database

--- Admin_Page/adminpage.php
+++ Admin_Page/adminpage.php
@@ -30,8 +30,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 </head>
     <title>admin</title>
 </head>
@@ -77,6 +76,7 @@
             $db->checkConnectionToDatabase();
             echo "<pre>";
             print_r($db->getTableByName("products"));
+            $db->insertRecordToProducts("cap", "t", "t", "t", "10.00");
             echo "</pre>";
 ?>
 </body>
