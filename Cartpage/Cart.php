76210462e03fae2a2d5c0ac3eae261e656f07f0f
Terique915 <54835492+Terique915@users.noreply.github.com>
2021-06-08 09:43:12 +0200

    Cart page

---
+++ Cartpage/Cart.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="../contact_pagina/style.css">
+    <style>
+        #checkout{
+            padding-left: 105px;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <nav id= "menu">
+        <ul>
+            <li><img id="bit-img" src="../img/logo.svg"></li>
+            <!--<li style="float: right; padding-right: 30px;" ><a href = "Contact.html" target = "_self">contact</a></li>-->
+            <li style = "float: right"><a href = "">basket</a></li>
+            <li style = "float: right"><a href = "">acount</a></li>
+            <li style = "float: right"><a href = "">search</a></li>
+            <li class = "dropdown" style="float: right;">
+                <a href = "javascript:void(0)" class="dropbtn">Categorie&#235;n</a>
+                <div class="dropdown-content">
+                    <a href="#">link 1</a>
+                    <a href="#">link 2</a>
+                </div>
+            </li>
+            <li style = "float: right"><a href="index.html" target = "_self">Shop</a></li>
+        </ul>
+    </nav>
+</header>
+<h2 id="checkout">Checkout</h2>
+<section class="details">
+<p>Billing Details</p>
+
+</section>
+<section class="Info">
+
+</section>
+</body>
+</html>
