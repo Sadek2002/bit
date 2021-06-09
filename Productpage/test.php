36770bae4cb1ada0ec0dc7d30b073208ed066f14
Sadek <39184773+Sadek2002@users.noreply.github.com>
2021-06-09 15:10:29 +0200

    Fixed description and changed img src

--- Productpage/test.php
+++ Productpage/test.php
@@ -42,7 +42,7 @@ $product = $db->getRecordsFromTable("products", "product_id", $_GET['id']);
 <div id="content" style="background-color:white">
 <div class="row">
     <div class="column" >
-        <?php echo "<img alt='' src='".$product[0]['img_url']."'>"?>
+        <?php echo "<img alt='' src='../".$product[0]['img_url']."'>"?>
     </div>
     <div class="column" >
 
@@ -64,7 +64,7 @@ $product = $db->getRecordsFromTable("products", "product_id", $_GET['id']);
         <input type="submit" id="button" value="Add to Cart">
         <br>
         <br>
-        <article><?php echo $product[0]['Description'] ?></article>
+        <article><?php echo $product[0]['description'] ?></article>
     </div>
 
 </div>
