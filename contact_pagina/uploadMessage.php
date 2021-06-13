5dc88ee9b8168b4030a15288c1d0053481797a13
alicia573 <54794745+alicia573@users.noreply.github.com>
2021-06-13 13:23:21 +0200

    update

--- contact_pagina/uploadMessage.php
+++ contact_pagina/uploadMessage.php
@@ -1,7 +1,7 @@
 <?php
 include_once ('../php/Database.php');
 
-$db = new Database('localhost','bit_academy','3306','root','alicia573');
+$db = new Database('localhost','bit_academy','3306','root','root');
 $db->checkConnectionToDatabase();
 
 if(isset($_POST['submit'])){
