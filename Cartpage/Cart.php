7a79e91a839c21b8c87010b1a017fbde17f66aca
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2021-06-23 13:13:14 +0200

    cart page has now delete buttons

--- Cartpage/Cart.php
+++ Cartpage/Cart.php
@@ -18,6 +18,12 @@
         header("location: Cart.php");
     }
 
+    if (isset($_GET['index'])) {
+        unset($_SESSION['product_list'][$_GET['index']]);
+        array_splice($_SESSION['product_list'], 0, count($_SESSION['product_list']), $_SESSION['product_list']);
+        header("location: Cart.php");
+    }
+
     /*echo "<pre>";
     print_r($_SESSION);
     echo "</pre>";*/
@@ -60,9 +66,10 @@
 
 
     //makes the ordered products
-    if(isset($_SESSION['product_list'])) {
+    if(isset($_SESSION['product_list']) && !empty($_SESSION['product_list'])) {
 
         $totalPrice = 0;
+        $i = 0;
 
         foreach ($_SESSION['product_list'] as $row) {
             echo "<div id='produktinfo'>";
@@ -77,18 +84,18 @@
                 echo '<p id="size">Size: ' . $row['size'] . "</p>";
             }
 
-            echo "<p id='price'>" . '€' . $product[0]['price'] * $row['quantity'] . "<br>"."</p>";
+            echo "<p id='price'>" . '€ ' . $product[0]['price'] * $row['quantity'] . "<br>"."</p>";
+
+            echo "<a id='cart-delete' href='Cart.php?index=".$i."'>delete</a>";
 
             echo "</div>";
             $calc = $product[0]['price'] * $row['quantity'];
             $totalPrice += $calc;
+            $i++;
         }
-
-
-    }
-    else{
+    } else {
         echo '<p id="empty_log">Cart is empty</p>';
-        $totalPrice =0;
+        $totalPrice = 0;
     }
     ?>
 </section>
